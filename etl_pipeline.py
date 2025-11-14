57ef45c69aa505bb674ea830019f45083f254429
anilremo23 <mastermind5767@gmail.com>
2025-11-14 21:36:36 +0530

    added logging

--- etl_pipeline.py
+++ etl_pipeline.py
@@ -10,19 +10,22 @@ from db import insert_chunk
 from config.config import GOOGLE_CREDENTIALS_FILE, EMBED_MODEL, CHUNK_SIZE
 
 def run_etl(service, file_id, file_name):
+    print("Download PDF file has been started")
     pdf_path = download_pdf(service, file_id, file_name)
     text = extract_text(pdf_path)
     chunks = list(chunk_text(text, CHUNK_SIZE))
     embedder = Embedder(EMBED_MODEL)
     embeddings = embedder.embed(chunks)
+    
     for i, (chunk, emb) in enumerate(zip(chunks, embeddings)):
         insert_chunk(file_name, i, chunk, emb)
     print("Done", file_name)
 
 if __name__ == "__main__":
     svc = authenticate_drive(GOOGLE_CREDENTIALS_FILE)
-    # replace with real file ids or implement a file-list fetcher
-    files = [{"id":"1DwOd0ZbjVU3nDv2QJABlcEUdDJWcgCwW", "name":"0321815734.pdf"}]
+    # file has been added to drive 
+    files = [{"id":"1DwOd0ZbjVU3nDv2QJABlcEUdDJWcgCwW", "name":"0321815734.pdf"},{"id":"1vyMu5gikAD5w-zit4t8BwwnJXWs5S59F","name":"SOFTWARE-DESIGN-BUDGEN.pdf"}]
     for f in files:
+        print(f'{f["name"]} Started')
         run_etl(svc, f["id"], f["name"])
     print("process completed")
