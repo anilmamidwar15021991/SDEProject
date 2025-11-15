2cb578aa6080fa0df851dc351618de38193a3a24
anilremo23 <mastermind5767@gmail.com>
2025-11-15 11:25:26 +0530

    retry logic added for google auth

--- drive_auth.py
+++ drive_auth.py
@@ -1,3 +1,4 @@
+from time import time
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseDownload
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -5,10 +6,26 @@ import io
 
 SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
 
-def authenticate_drive(credentials_path):
-    flow = InstalledAppFlow.from_client_secrets_file(credentials_path, SCOPES)
-    creds = flow.run_local_server(port=0)  # opens browser to authorize
-    return build('drive', 'v3', credentials=creds)
+def authenticate_drive(credentials_path, max_retries=3, retry_delay=2):
+    retries = 0
+
+    while retries < max_retries:
+        try:
+            print(f"Authenticating with Google Drive... Attempt {retries + 1}/{max_retries}")
+            flow = InstalledAppFlow.from_client_secrets_file(credentials_path, SCOPES)
+            creds = flow.run_local_server(port=0)
+            print("Authentication successful.")
+            return build('drive', 'v3', credentials=creds)
+
+        except Exception as e:
+            print(f"Authentication failed: {e}")
+            retries += 1
+            if retries < max_retries:
+                print(f"Retrying in {retry_delay} seconds...")
+                time.sleep(retry_delay)
+            else:
+                print("Max retries reached. Could not authenticate with Google Drive.")
+                raise
 
 def download_pdf(service, file_id, dest_path):
     request = service.files().get_media(fileId=file_id)
--- etl_pipeline.py
+++ etl_pipeline.py
@@ -1,5 +1,6 @@
 
 import sys, os
+import time
 print("python:", sys.executable)
 print("cwd   :", os.getcwd())
 
@@ -13,9 +14,17 @@ def run_etl(service, file_id, file_name):
     print("Download PDF file has been started")
     pdf_path = download_pdf(service, file_id, file_name)
     text = extract_text(pdf_path)
+    start_chunk=time.time()
+    
     chunks = list(chunk_text(text, CHUNK_SIZE))
+    print(f'Total Chunks-{len(chunks)}')
+    end_chunk = time.time()
+    print(f"Chunking Time: {end_chunk - start_chunk:.4f} seconds")
     embedder = Embedder(EMBED_MODEL)
+    start_embed = time.time()
     embeddings = embedder.embed(chunks)
+    end_embed = time.time()
+    print(f"Embedding Time: {end_embed - start_embed:.4f} seconds")
     
     for i, (chunk, emb) in enumerate(zip(chunks, embeddings)):
         insert_chunk(file_name, i, chunk, emb)
